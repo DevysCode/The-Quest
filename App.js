7a04726f604fca60974a7a715a91dc3312a7fd2b
Devy <166741518+DevysCode@users.noreply.github.com>
2024-08-30 01:31:26 -0500

    Added eyes and basic styling

--- App.js
+++ App.js
@@ -3,19 +3,29 @@ const characters = [
     health:80,
     baseAttack: 2,
     skill: "Long Shot",
-    skillAttack: 4
+    skillAttack: 6,
+    skillPoints: 20
   },
   Thief = {
     health:100,
     baseAttack: 6,
     skill: "Back Stab",
-    skillAttack: 8
+    skillAttack: 8,
+    skillPoints: 20
   },
   Warrior = {
     health:150,
     baseAttack: 4,
     skill: "Long Sword",
-    skillAttack: 6
+    skillAttack: 6,
+    skillPoints: 20
+  },
+  Mage = {
+    health:100,
+    baseAttack: 3,
+    skill: "Fire Ball",
+    skillAttack: 6,
+    skillPoints: 20
   }
 ]
 
