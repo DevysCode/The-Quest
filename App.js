78a2486ee2e453976967748242368ab63310f25e
Devy <166741518+DevysCode@users.noreply.github.com>
2024-09-02 18:20:44 -0500

    Added buttons for testing

--- App.js
+++ App.js
@@ -1,5 +1,27 @@
+let selection = document.getElementById('select_fighter')
+let theArcher = document.getElementById('Archer')
+let theThief = document.getElementById('Thief')
+let theWarrior = document.getElementById('Warrior')
+let theMage = document.getElementById('Mage')
+
+console.log(selection)
+
+theArcher.addEventListener("click", function(){
+  console.log("Archer")
+})
+theThief.addEventListener("click", function(){
+  console.log("Thief")
+})
+theWarrior.addEventListener("click", function(){
+  console.log("Warrior")
+})
+theMage.addEventListener("click", function(){
+  console.log("Mage")
+})
+
 const characters = [
   Archer = {
+    name : "Archer",
     health:80,
     baseAttack: 2,
     skill: "Long Shot",
@@ -7,6 +29,7 @@ const characters = [
     skillPoints: 20
   },
   Thief = {
+    name : "Thief",
     health:100,
     baseAttack: 6,
     skill: "Back Stab",
@@ -14,6 +37,7 @@ const characters = [
     skillPoints: 20
   },
   Warrior = {
+    name : "Warrior",
     health:150,
     baseAttack: 4,
     skill: "Long Sword",
@@ -21,6 +45,7 @@ const characters = [
     skillPoints: 20
   },
   Mage = {
+    name : "Mage",
     health:100,
     baseAttack: 3,
     skill: "Fire Ball",
@@ -28,7 +53,8 @@ const characters = [
     skillPoints: 20
   }
 ]
+console.log(characters[0].name)
 
-const select = characters.map(function(character){
+/*const select = characters.map(function(character){
  console.log(character)
-})
+})*/
